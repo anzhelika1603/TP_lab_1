d7f4c45c59884c75a4c23d6128eec85cfa4b8bbb
anzhelika1603 <anzhelica1603@rambler.ru>
2021-11-09 13:41:57 +0300

    реализован дек

--- TP_1_11/Deque.h
+++ TP_1_11/Deque.h
@@ -2,12 +2,14 @@
 #include "Queue.h"
 
 template <class T>
-class Stack : private Queue<T>
+class Deque : private Queue<T>
 {
 public:
-	Stack() : Queue<T>() {}
-	Stack(const Stack<T>& stack) : Queue<T>(stack) {}
+	Deque() : Queue<T>() {};
+	Deque(const Deque<T>& deque) : Queue<T>(deque) {}
+	void push_font(T data);
 	void push_back(T data) { Queue<T>::push_back(data); }
+	T pop_front() { return Queue<T>::pop_front(); }
 	T pop_back();
 	void clear() { Queue<T>::clear(); }
 	bool isEmpty() { return Queue<T>::isEmpty(); }
@@ -15,19 +17,27 @@ public:
 	void printToFile(ofstream& out) { Queue<T>::printToFile(); }
 	void inputFromFile(ifstream& out) { Queue<T>::inputFromFile(); }
 	void inputFromConsole() { Queue<T>::inputFromConsole(); }
-	void merge(Stack<T>& stack) { Queue<T>::merge(stack); }
+	void merge(Deque<T>& deque) { Queue<T>::merge(deque); }
 	void getSize() { Queue<T>::getSize(); }
 
-	Stack<T>& operator=(const Stack<T>& stack);
+	Deque<T>& operator=(const Deque<T>& deque);
 };
 
 template <class T>
-T Stack<T>::pop_back() {
-	string err = "Стек пуст, нечего извлекать";
+void Deque<T>::push_font(T data) {
+	Deque<T> tmp;
+	tmp.push_back(data);
+	tmp.merge(*this);
+	*this = tmp;
+}
+
+template <class T>
+T Deque<T>::pop_back() {
+	string err = "Дек пуст, нечего извлекать";
 	if (isEmpty())
 		throw err;
 	T data = Queue<T>::tail->data;
-	Stack<T> tmp;
+	Deque<T> tmp;
 	for (int i = 0; i < Queue<T>::size - 1; i++)
 	{
 		Element<T>* e = Queue<T>::tail;
@@ -40,9 +50,9 @@ T Stack<T>::pop_back() {
 }
 
 template <class T>
-void Stack<T>::printToConsole() {
+void Deque<T>::printToConsole() {
 	if (Queue<T>::size == 0)
-		cout << "Стек пуст";
+		cout << "Дек пуст";
 	else
 		for (int i = 0; i < Queue<T>::size; i++)
 		{
@@ -55,17 +65,17 @@ void Stack<T>::printToConsole() {
 }
 
 template <class T>
-Stack<T>& Stack<T>::operator=(const Stack<T>& stack)
+Deque<T>& Deque<T>::operator=(const Deque<T>& deque)
 {
-	if (this == &stack)
+	if (this == &deque)
 		return *this;
 	Queue<T>::tail = NULL;
 	Queue<T>::size = 0;
 	clear();
-	for (int i = 0; i < stack.size; i++)
+	for (int i = 0; i < deque.size; i++)
 	{
-		Element<T>* e = stack.tail;
-		for (int j = 0; j < stack.size - i - 1; j++)
+		Element<T>* e = deque.tail;
+		for (int j = 0; j < deque.size - i - 1; j++)
 			e = e->prev;
 		push_back(e->data);
 	}
--- TP_1_11/main.cpp
+++ TP_1_11/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Stack.h"
+#include "Deque.h"
 
 using namespace std;
 
@@ -7,16 +8,17 @@ int main()
 {
     setlocale(LC_ALL, "Rus");
 
-    Stack<int> a, b;
+    Deque<int> a, b;
     a.push_back(1);
     a.push_back(2);
     a.push_back(3);
+    a.push_font(4);
     try
     {
-       // a.merge(a);
+        a.merge(a);
         a.printToConsole();
-        cout << a.pop_back() << endl;
-        cout << a.pop_back() << endl;
+        cout << a.pop_front() << endl;
+        cout << a.pop_front() << endl;
         cout << a.pop_back() << endl;
         cout << a.pop_back() << endl;
         cout << a.pop_back() << endl;
